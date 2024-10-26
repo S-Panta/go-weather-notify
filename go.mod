75e833141bb712dd6e53911f3875bc5a2e65087c
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2024-10-26 15:39:56 +0545

    setup rocketchat (#5)

--- go.mod
+++ go.mod
@@ -1,23 +1,15 @@
 module go-weather-notify
 
-go 1.22.1
+go 1.23.1
 
 require (
-	github.com/briandowns/openweathermap v0.20.0
+	github.com/RocketChat/Rocket.Chat.Go.SDK v0.0.0-20240116134246-a8cbe886bab0
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
-	github.com/badkaktus/gorocket v0.1.4 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gen2brain/beeep v0.0.0-20240516210008-9c006672e7f4 // indirect
-	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
-	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/Jeffail/gabs v1.4.0 // indirect
+	github.com/gopackage/ddp v0.0.0-20170117053602-652027933df4 // indirect
+	github.com/sony/sonyflake v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
 )
--- server.go
+++ server.go
@@ -1,6 +1,164 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/realtime"
+)
+
+type WeatherData struct {
+	Condition   string `json:"main"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+}
+
+type Temperature struct {
+	Temp      float64 `json:"temp"`
+	FeelsLike float64 `json:"feels_like"`
+	TempMin   float64 `json:"temp_min"`
+	TempMax   float64 `json:"temp_max"`
+}
+type WeatherResponse struct {
+	Weather  []WeatherData `json:"weather"`
+	Main     Temperature   `json:"main"`
+	CityName string        `json:"name"`
+}
+
+type Config struct {
+	ApiKey    string
+	ServerUrl string
+	Username  string
+	Password  string
+	Channel   string
+}
+
+func GetConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	apiKey := os.Getenv("OPEN_WEATHER_API_KEY")
+	serverURL := os.Getenv("ROCKETCHAT_SERVER_URL")
+	username := os.Getenv("USER_NAME")
+	password := os.Getenv("USER_PASSWORD")
+	channel := os.Getenv("ROCKETCHAT_CHANNEL")
+
+	return Config{
+		ApiKey:    apiKey,
+		ServerUrl: serverURL,
+		Username:  username,
+		Password:  password,
+		Channel:   channel,
+	}
+
+}
+func GetWeatherData(apiKey string, city string) (WeatherResponse, error) {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	baseUrl := "https://api.openweathermap.org/data/2.5/weather?appid=%s&q=%s&units=metric"
+
+	url := fmt.Sprintf(baseUrl, apiKey, city)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		fmt.Println("Expected status code be 200 but received:", res.StatusCode)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+	}
+
+	var response WeatherResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+
+	}
+	return response, nil
+}
+
 func main() {
-	// GetWeatherData()
-	InitializeChat()
+	config := GetConfig()
+
+	u, err := url.Parse(config.ServerUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := realtime.NewClient(u, true)
+	if err != nil {
+		log.Fatalf("Failed to create real-time client: %v", err)
+	}
+
+	if _, err := client.Login(&models.UserCredentials{Email: config.Username, Password: config.Password}); err != nil {
+		log.Fatalf("Login failed: %v", err)
+	}
+	fmt.Println("Logged in successfully!")
+	roomId, err := client.GetChannelId(config.Channel)
+	if err != nil {
+		fmt.Println("error getting room id", err)
+	}
+	messageChannel := make(chan models.Message)
+
+	if err := client.SubscribeToMessageStream(&models.Channel{Name: config.Channel, ID: roomId}, messageChannel); err != nil {
+		log.Fatalf("Failed to subscribe to room: %v", err)
+	}
+	fmt.Println("Subscribed to message stream for room:", config.Channel, roomId)
+
+	go func() {
+		for msg := range messageChannel {
+			fmt.Printf("Received message %s\n", msg.Msg)
+
+			if strings.HasPrefix((msg.Msg), "weather") {
+				words := strings.Fields(msg.Msg)
+				cityName := words[1]
+				response, err := GetWeatherData(config.ApiKey, cityName)
+				if err != nil {
+					fmt.Println("Function call error")
+				}
+				condition := response.Weather[0].Condition
+				description := response.Weather[0].Description
+				feelsLike := response.Main.FeelsLike
+				maxTemp := response.Main.TempMax
+				icon := response.Weather[0].Icon
+				city := response.CityName
+
+				iconURL := fmt.Sprintf("https://openweathermap.org/img/wn/%s.png", icon)
+				msg := fmt.Sprintf("Weather Update:\nCondition: %s\nDescription: %s\nFeels Like: %.2f°C\nMax Temperature: %.2f°C \n City: %s \n Status: ![Weather Icon](%s)",
+					condition, description, feelsLike, maxTemp, city, iconURL)
+
+				reply := &models.Message{
+					RoomID: roomId,
+					Msg:    msg,
+				}
+
+				go func(reply *models.Message) {
+					if _, err := client.SendMessage(reply); err != nil {
+						log.Printf("Failed to send reply message: %v", err)
+					}
+				}(reply)
+			}
+
+		}
+
+	}()
+	select {}
 }
