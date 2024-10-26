8e0522f0969b3b1a6c5f2e83aa5ff5b8e98aafe3
unknown <sabinpanta11@gmail.com>
2024-10-26 15:55:04 +0545

    seperated file for seperate responsibiklity

--- server.go
+++ server.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"net/url"
 	"os"
 	"strings"
@@ -61,38 +58,6 @@ func GetConfig() Config {
 		Channel:   channel,
 	}
 
-}
-func GetWeatherData(apiKey string, city string) (WeatherResponse, error) {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	baseUrl := "https://api.openweathermap.org/data/2.5/weather?appid=%s&q=%s&units=metric"
-
-	url := fmt.Sprintf(baseUrl, apiKey, city)
-
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		fmt.Println("Expected status code be 200 but received:", res.StatusCode)
-	}
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-	}
-
-	var response WeatherResponse
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
-
-	}
-	return response, nil
 }
 
 func main() {
