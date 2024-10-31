c8284f59d1d5ae277735d5826ed92cdfa6cb1d10
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2024-10-31 12:57:02 +0545

    basic kubernetes setup (#10)

--- server.go
+++ server.go
@@ -40,9 +40,11 @@ type Config struct {
 }
 
 func GetConfig() Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 	apiKey := os.Getenv("OPEN_WEATHER_API_KEY")
 	serverURL := os.Getenv("ROCKETCHAT_SERVER_URL")
